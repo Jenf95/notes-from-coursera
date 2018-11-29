642c28ac7507cb93f898a8a603b4e83eb94ccc9c
Jenf95 <40010506+Jenf95@users.noreply.github.com>
2018-11-29 14:11:16 +0800

    Update Statistical Analysis.py

--- Statistical Analysis.py
+++ Statistical Analysis.py
@@ -30,3 +30,38 @@ stats.ttest_ind(early['assignment1_grade'], late['assignment1_grade'])
 #Assignment: University towns have their mean housing prices less effected by recessions. Run a t-test to compare the ratio of the mean 
       #price of the houses in university towns the quarter before the recession starts compared to the receccion bottom 
       #(price_ratio = quarter_before_recession/recession_bottom
+        
+def get_list_of_university_towns():
+    '''Returns a DataFrame of towns and the states they are in from the 
+    university_towns.txt list. The format of the DataFrame should be:
+    DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ], 
+    columns=["State", "RegionName"]  )
+    
+    The following cleaning needs to be done:
+
+    1. For "State", removing characters from "[" to the end.
+    2. For "RegionName", when applicable, removing every character from " (" to the end.
+    3. Depending on how you read the data, you may need to remove newline character '\n'. '''
+    university_towns = []
+    state = None
+    towns = [] 
+    file = open('university_towns.txt',"r")
+    for line in file:
+        new_line = line[:-1]
+        if new_line[-6:] == '[edit]':
+            state = new_line[:-6]
+            continue
+        if '(' in line:
+            town = new_Line[:thisLine.index('(')-1]
+            towns.append([state,town])
+        else:
+            town = new_line
+            towns.append([state,town])
+    university_towns = pd.DataFrame(towns, columns = ['State','RegionName'])
+    return university_towns
+
+get_list_of_university_towns()
+#this returns an empty dataframe, figure out why
+        
+        
+        
