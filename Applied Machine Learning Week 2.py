4b6e76913e7be8bd52789c9ce05e3cbb2f3c35ed
Jenf95 <40010506+Jenf95@users.noreply.github.com>
2019-07-04 21:48:01 +0800

    Update Applied Machine Learning Week 2.py

--- Applied Machine Learning Week 2.py
+++ Applied Machine Learning Week 2.py
@@ -22,10 +22,12 @@ whereas a score of 1 corresponds to perfect prediction'''
 from sklearn.linear_model import Ridge
 X_train, X_test, y_train, y_test = train_test_split(X_crime, y_crime, random_state = 0)
 linridge=Ridge(alpha=20.0).fit(X_train, y_train) 
-'''the influence of the regularization term is controled by alpha; higher alpha means more regularization
+'''
+The influence of the regularization term is controled by alpha; higher alpha means more regularization
 and simpler models
 L2 penalty is the sum of squares for all the coefficients
-Feature normalization technique: MinMax scaling(transform all the values to between 0 and 1)'''
+Feature normalization technique: MinMax scaling(transform all the values to between 0 and 1)
+'''
 from sklearn.preprocessing import MinMaxScaler
 scaler=MinMaxScaler
 scaler.fit(X_train)
@@ -35,6 +37,14 @@ X_test_scaled = scaler.transform(X_test)
 X_train_scaled = scalter.fit_transform(X_train)
 clf = Ridge().fit(X_train_scaled, y_train)
 r2_score = clf.score(X_test_scaled, y_test)
-'''the test set must use identical scaling to the training set
+'''the test set must use identical scaling to the training set'''
 
 #Lasso Regression
+'''
+L1 penalty is to minimize the sum of the absolute values of the coefficients
+This has the effect of setting parameter weights to zero for the least influential variables. It's called 
+sparse solution: a kind of feature selection.
+Parameter alpha in the function controls L1 penalty (default = 1.0)
+Ridge vs. Lasso:
+- Many small to medium sized effects: use Ridge
+- Only a few variables with medium/large effect: Use Lasso
